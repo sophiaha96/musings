12e48bf98c6526d83426f4352c291e4717e686e6
Sophia Ha <sophia.ha@tetratrustcompany.com>
2021-03-25 02:44:13 -0600

    feat: individual posts page

---
+++ src/Components/PostPage/index.tsx
@@ -0,0 +1,32 @@
+import React, { Fragment } from 'react'
+import useStyles from './theme'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid';
+import { RouteComponentProps } from '@reach/router'
+import IPost, { defaultPost } from '../../Posts/interface'
+
+
+export default function MainPage(props: RouteComponentProps<{location: {state: { post: IPost }}}>/* , post: IPost */): JSX.Element {
+  const classes = useStyles()
+  const post: IPost = props.location?.state.post ?? defaultPost
+
+  return (
+    <Fragment key={post.id}>
+      <Grid container className={classes.postRoot}>
+        <Grid item xs={11} sm={9} md={6} lg={4} xl={4}>
+          <div className={classes.space}>
+            <Paper elevation={3} className={classes.paper}>
+            <Typography variant="caption" align="right">{post.date}</Typography>
+            <Typography variant="h3" align="left" gutterBottom>{post.title}</Typography>
+            <Typography variant="h6" gutterBottom>{post.subtitle}</Typography>
+            <Typography variant="body1" style={{ whiteSpace: 'pre-line' }}>
+              {post.body}
+            </Typography>
+          </Paper>
+          </div>
+        </Grid>
+      </Grid>
+    </Fragment>
+  )
+}
---
+++ src/Components/PostPage/theme.tsx
@@ -0,0 +1,33 @@
+import { makeStyles } from '@material-ui/core/styles'
+import { COLORS } from '../../MuiTheme'
+
+const useStyles = makeStyles((theme) => ({
+  
+  root: {
+    display: 'flex',
+    height: '100%',
+    width: '100vw',
+    
+  },
+
+  postRoot: {
+    justifyContent: 'center',
+  },
+
+  paper: {
+    display: 'grid',
+    gridTemplateRows: '.5fr 2fr 2fr auto',
+    height: '100%',
+    padding: theme.spacing(5),
+    marginTop: theme.spacing(5),
+    backgroundColor: COLORS.PAPER,
+  },
+
+  space: {
+    marginBottom: theme.spacing(5),
+    paddingBottom: theme.spacing(10),
+  }
+
+}))
+
+export default useStyles
