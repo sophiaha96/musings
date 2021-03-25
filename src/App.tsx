a1d5221476b80886f4412b24c5868effdbc83d9e
Sophia Ha <sophia.ha@tetratrustcompany.com>
2021-03-25 02:44:25 -0600

    feat: application

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { Router } from '@reach/router';
+import { MuiThemeProvider } from '@material-ui/core';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from './MuiTheme'
+import Header from './Components/Header'
+import Navigate from './Components/Navigate'
+import MainPage from './Components/MainPage';
+import PostPage from './Components/PostPage'
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+        <Navigate />
+        <Router>
+          <MainPage path='/' />
+          <PostPage path="/:title/:id" />
+      </Router>
+      </MuiThemeProvider>
+   </Fragment>
   );
 }
 
